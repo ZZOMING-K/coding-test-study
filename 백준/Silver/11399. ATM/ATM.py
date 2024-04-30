cf0a54a64ec5dfdf4edc761e8742bac909f4df3c
ZZOMING-K <89764752+ZZOMING-K@users.noreply.github.com>
2024-04-30 18:36:47 +0900

    [Silver IV] Title: ATM, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,14 +1,14 @@
-N = int(input())
-P_list = list(map(int,input().split())) 
+N = int(input()) #사람 수 
+P_list = list(map(int,input().split())) #i번 사람이 돈을 인출하는데 걸리는 시간  
 
-#시간 오름차순 정렬 
+#오름차순으로 정렬 
 P_list = sorted(P_list) 
 
-time = 0 
-sum_time = 0 
+time = 0 #i번 사람이 줄을 서서 돈을 인출하는데 걸리는 시간 
+sum_time = [] #각 사람이 돈을 인출하는데 필요한 시간의 합 
 
 for p in P_list : 
     time += p 
-    sum_time += time 
+    sum_time.append(time) 
 
-print(sum_time)
+print(sum(sum_time))
