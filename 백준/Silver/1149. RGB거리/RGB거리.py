c156c7d2da6158b03fe1352cbd564d252cece644
ZZOMING-K <89764752+ZZOMING-K@users.noreply.github.com>
2024-06-03 02:42:42 +0900

    [Silver I] Title: RGB거리, Time: 84 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,18 @@
+#DP 테이블 초기화 
+N = int(input())
+dp = [[0] * 3 for _ in range(N)]
+
+arr = []
+for _ in range(N) :
+	arr.append(list(map(int,input().split()))) 
+
+dp[0][0] = arr[0][0]
+dp[0][1] = arr[0][1]
+dp[0][2] = arr[0][2] 
+
+for i in range(1,N) : 
+    dp[i][0] = min(dp[i-1][1] , dp[i-1][2]) + arr[i][0] 
+    dp[i][1] = min(dp[i-1][0] , dp[i-1][2]) + arr[i][1] 
+    dp[i][2] = min(dp[i-1][0] , dp[i-1][1]) + arr[i][2] 
+
+print(min(dp[N-1][0] , dp[N-1][1] , dp[N-1][2]))
