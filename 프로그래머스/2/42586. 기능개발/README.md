5d8b2e3fbdfcbb97f0b48889bef120e203c38819
ZZOMING-K <89764752+ZZOMING-K@users.noreply.github.com>
2024-07-10 17:31:11 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.01 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 1:45:24
+2024년 07월 10일 17:31:07
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,22 +1,26 @@
 def solution(progresses, speeds):
     
-    answer = []
-    time = 0
-    count = 0
+    day_list = []
     
-    while len(progresses) > 0 :
-        
-        if progresses[0] + time * speeds[0] >= 100 :
-            progresses.pop(0) #첫번째 원소 없애기
-            speeds.pop(0) 
-            count += 1 
-        
+    for i in range(len(speeds)) : 
+        # 100% 달성하기 까지 걸리는기간 리스트 생성 
+        if (100 - progresses[i]) % speeds[i] == 0 : 
+            day_list.append((100 - progresses[i]) // speeds[i])
+        else : 
+            day_list.append(((100 - progresses[i]) // speeds[i]) +1)
+    
+    count = 1
+    result = []
+    max_day = day_list[0] 
+    
+    for j in range(1,len(day_list)) : 
+        if day_list[j] <= max_day : 
+            count+= 1 
         else :
-            if count > 0 :
-                answer.append(count)
-                count = 0 
-            time += 1 
-        
-    answer.append(count)
-    return answer
-        
+            result.append(count)
+            count = 1 #count 초기화 
+            max_day = max(max_day , day_list[j])
+            #time += 1 #time 넘기기 
+    result.append(count)
+    
+    return result 
